af81824c1b5a97a3b73b6cc6b3704310233ef730
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2019-11-14 12:33:11 -0500

    Fixed incrementer bug

--- rgbgame.js
+++ rgbgame.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 
     $(".modes").click(() => {
         $(".menu").toggle();
-        $(".target-color").toggle();
-        $(".user-colors").toggle();
+        $(".target-color").show();
+        $(".user-colors").show();
     });
 
     let red;
@@ -17,80 +17,115 @@ $(document).ready(function() {
     let greenHead = document.getElementById("green-h2");
     let blueHead = document.getElementById("blue-h2");
 
+    //let easyMaxVal = 50;
+    //let medMaxVal = 150;
+    //let hardMaxVal = 255;
+
+    //let redRand;
+    //let greenRand;
+    //let blueRand;
+
+    $(".modes:nth-of-type(1)").click(() => {
+        chooseMode(50);
+    });
+
+    $(".modes:nth-of-type(2)").click(() => {
+        chooseMode(150);
+    });
+
+    $(".modes:nth-of-type(3)").click(() => {
+        chooseMode(255);
+    });
+
+    $("#back-link").click(() => {
+        window.location.reload();
+        /*$(".menu").toggle();
+        $(".target-color").toggle();
+        $(".user-colors").toggle();
+
+        $(".modes:nth-of-type(1)").click(() => {
+            chooseMode(50);
+        });
+    
+        $(".modes:nth-of-type(2)").click(() => {
+            chooseMode(150);
+        });
+    
+        $(".modes:nth-of-type(3)").click(() => {
+            chooseMode(255);
+        });*/
+    });
+
+    //let redRand = Math.floor(Math.random() * (maxVal + 1));
+    //let greenRand = Math.floor(Math.random() * (maxVal + 1));
+    //let blueRand = Math.floor(Math.random() * (maxVal + 1));
+
     function chooseMode(maxVal) {
+    //$(".modes").click((maxVal) => {
+        let redRand = Math.floor(Math.random() * (maxVal + 1));
+        let greenRand = Math.floor(Math.random() * (maxVal + 1));
+        let blueRand = Math.floor(Math.random() * (maxVal + 1));
+        let rgbRand = `rgb(${redRand}, ${greenRand}, ${blueRand})`;
+
         red = maxVal;
         green = maxVal;
         blue = maxVal;
 
-        let redRand = Math.floor(Math.random() * (maxVal + 1));
-        let greenRand = Math.floor(Math.random() * (maxVal + 1));
-        let blueRand = Math.floor(Math.random() * (maxVal + 1));
-    
-        console.log(redRand);
-        console.log(greenRand);
-        console.log(blueRand);
+        redHead.innerHTML = `rgb(${red}, `;
+        greenHead.innerHTML = `${green}, `;
+        blueHead.innerHTML = `${blue})`;
 
-        $(".target-color").css("background-color", `rgb(${redRand}, ${greenRand}, ${blueRand})`);
+        $(".target-color").css("background-color", rgbRand);
+        $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
+
+        console.log(rgbRand);
 
         $("#red-add").click(() => {
             if (red < maxVal) {
-                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 red += 1;
+                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 redHead.innerHTML = "rgb(" + red + ", ";
             }
         });
     
         $("#green-add").click(() => {
             if (green < maxVal) {
-                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 green += 1;
+                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 greenHead.innerHTML = green + ", ";
             }
         });
     
         $("#blue-add").click(() => {
             if (blue < maxVal) {
-                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 blue += 1;
+                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 blueHead.innerHTML = blue + ")";
             }
         });
     
         $("#red-minus").click(() => {
             if (red > 0) {
-                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 red -= 1;
+                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 redHead.innerHTML = "rgb(" + red + ", ";
             }
         });
     
         $("#green-minus").click(() => {
             if (green > 0) {
-                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 green -= 1;
+                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 greenHead.innerHTML = green + ", ";
-                //$("body").css("background-color", "blue");
             }
         });
         
         $("#blue-minus").click(() => {
             if (blue > 0) {
-                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 blue -= 1;
+                $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 blueHead.innerHTML = blue + ")";
             }
         });
     }
-
-    $(".modes:nth-of-type(1)").click(() => {
-        chooseMode(50);
-    })
-
-    $(".modes:nth-of-type(2)").click(() => {
-        chooseMode(150);
-    })
-
-    $(".modes:nth-of-type(3)").click(() => {
-        chooseMode(255);
-    })
 });
