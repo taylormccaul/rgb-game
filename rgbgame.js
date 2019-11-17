1ecc806f6ee5ba7926b72115208ccb400d035233
taylormccaul <31233095+taylormccaul@users.noreply.github.com>
2019-11-16 20:21:12 -0500

    Add win screen, iterator

--- rgbgame.js
+++ rgbgame.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $(".target-color").hide();
     $(".user-colors").hide();
     $(".menu").show();
+    $(".win-screen").hide();
 
     $(".modes").click(() => {
         $(".menu").toggle();
@@ -17,13 +18,9 @@ $(document).ready(function() {
     let greenHead = document.getElementById("green-h2");
     let blueHead = document.getElementById("blue-h2");
 
-    //let easyMaxVal = 50;
-    //let medMaxVal = 150;
-    //let hardMaxVal = 255;
+    let inc = 1;
 
-    //let redRand;
-    //let greenRand;
-    //let blueRand;
+    $("#number-field").defaultValue = 1;
 
     $(".modes:nth-of-type(1)").click(() => {
         chooseMode(50);
@@ -39,21 +36,20 @@ $(document).ready(function() {
 
     $("#back-link").click(() => {
         window.location.reload();
-        /*$(".menu").toggle();
-        $(".target-color").toggle();
-        $(".user-colors").toggle();
+    });
 
-        $(".modes:nth-of-type(1)").click(() => {
-            chooseMode(50);
-        });
-    
-        $(".modes:nth-of-type(2)").click(() => {
-            chooseMode(150);
-        });
-    
-        $(".modes:nth-of-type(3)").click(() => {
-            chooseMode(255);
-        });*/
+    $("#inc-plus").click(() => {
+        if (inc >= 0) {
+            inc += 1;
+            document.querySelector(".iterator-div > h4").innerHTML = inc;
+        }
+    });
+
+    $("#inc-minus").click(() => {
+        if (inc > 0) {
+            inc -= 1;
+            document.querySelector(".iterator-div > h4").innerHTML = inc;
+        }
     });
 
     //let redRand = Math.floor(Math.random() * (maxVal + 1));
@@ -82,50 +78,71 @@ $(document).ready(function() {
 
         $("#red-add").click(() => {
             if (red < maxVal) {
-                red += 1;
+                red += inc;
                 $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 redHead.innerHTML = "rgb(" + red + ", ";
+                alertWin();
             }
         });
     
         $("#green-add").click(() => {
             if (green < maxVal) {
-                green += 1;
+                green += inc;
                 $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 greenHead.innerHTML = green + ", ";
+                alertWin();
             }
         });
     
         $("#blue-add").click(() => {
             if (blue < maxVal) {
-                blue += 1;
+                blue += inc;
                 $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 blueHead.innerHTML = blue + ")";
+                alertWin();
             }
         });
     
         $("#red-minus").click(() => {
             if (red > 0) {
-                red -= 1;
+                red -= inc;
                 $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 redHead.innerHTML = "rgb(" + red + ", ";
+                alertWin();
             }
         });
     
         $("#green-minus").click(() => {
             if (green > 0) {
-                green -= 1;
+                green -= inc;
                 $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 greenHead.innerHTML = green + ", ";
+                alertWin();
             }
         });
         
         $("#blue-minus").click(() => {
             if (blue > 0) {
-                blue -= 1;
+                blue -= inc;
                 $(".user-colors").css("background-color", `rgb(${red}, ${green}, ${blue})`);
                 blueHead.innerHTML = blue + ")";
+                alertWin();
             }
         });
+
+        function alertWin() {
+            if (red === redRand && green === greenRand && blue === blueRand) {
+                $(".menu").hide();
+                $(".target-color").hide();
+                $(".user-colors").hide();
+                document.getElementById("value-show").innerHTML = `YOU WON! The value was ${rgbRand}`;
+                $(".win-screen").show();
+
+                $("#play-again").click(() => {
+                    $(".menu").toggle();
+                    $(".win-screen").toggle();
+                });
+            }
+        }
     }
 });
